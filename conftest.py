0840d986527a257f8899278366235886c3e28814
Dhaval Savalia <hello@dhavalsavalia.com>
2025-04-19 13:41:32 -0400

    feat: start ORM
    Signed-off-by: Dhaval Savalia <hello@dhavalsavalia.com>

--- conftest.py
+++ conftest.py
@@ -1,6 +1,9 @@
+import os
+
 import pytest
 
 from plinx import Plinx
+from plinx.orm import Database
 
 
 # Application fixtures
@@ -12,3 +15,14 @@ def app():
 @pytest.fixture
 def client(app):
     return app.test_session()
+
+# ORM fixtures
+@pytest.fixture
+def db():
+    DB_PATH = "./test.db"
+    if os.path.exists(DB_PATH):
+        os.remove(DB_PATH)
+    db = Database(DB_PATH)
+    yield db
+    db.close()
+    return db
